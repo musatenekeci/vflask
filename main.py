cc7e46beaad45bc5fcdca18fe6dbeb59a8ba0bbe
musatenekeci <119789647+musatenekeci@users.noreply.github.com>
2023-04-02 21:46:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Musa Flask World!'
+    return 'Hello Musa Flask OPENAI World!'
 
 
 if __name__ == '__main__':
